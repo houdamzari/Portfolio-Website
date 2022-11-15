1ceace519544f8c01d82996e8163ce15d37260aa
houdamzari <houdamzari1999@gmail.com>
2022-11-15 11:42:02 +0100

    Fix eslint errors

--- script/script.js
+++ script/script.js
@@ -1,21 +1,21 @@
-const navWrapper = document.querySelector(".hamburger-menu");
-const hamMenu = document.querySelectorAll(".line");
-const sideNav = document.querySelector(".side-nav");
-const sideLinks = document.querySelectorAll(".links");
-
+/* eslint-disable linebreak-style */
+const navWrapper = document.querySelector('.hamburger-menu');
+const hamMenu = document.querySelectorAll('.line');
+const sideNav = document.querySelector('.side-nav');
+const sideLinks = document.querySelectorAll('.links');
 const Toggler = (e) => {
   e.preventDefault();
-  hamMenu.forEach((a) => a.classList.toggle("active"));
-  sideNav.classList.toggle("hide");
+  hamMenu.forEach((a) => a.classList.toggle('active'));
+  sideNav.classList.toggle('hide');
 };
 
-navWrapper.addEventListener("click", Toggler);
+navWrapper.addEventListener('click', Toggler);
 
+// eslint-disable-next-line no-plusplus
 for (let i = 0; i < sideLinks.length; i++) {
-  sideLinks[i].addEventListener("click", (e) => {
+  sideLinks[i].addEventListener('click', (e) => {
     const id = sideLinks[i].href.slice(23, sideLinks[i].href.length);
     Toggler(e);
     document.getElementById(id).scrollIntoView();
-    console.log(id);
   });
 }
