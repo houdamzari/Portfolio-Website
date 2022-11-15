b7266a05b6570dcdc1dfab7dc7acf8ffe708f990
Ibranista <ibranista88@gmail.com>
2022-11-15 10:05:51 +0100

    tried to fix the issue.

--- script/script.js
+++ script/script.js
@@ -1,6 +1,8 @@
 const navWrapper = document.querySelector(".hamburger-menu");
 const hamMenu = document.querySelectorAll(".line");
 const sideNav = document.querySelector(".side-nav");
+const sideLinks = document.querySelectorAll(".links");
+
 const Toggler = (e) => {
   e.preventDefault();
   hamMenu.forEach((a) => a.classList.toggle("active"));
@@ -8,3 +10,11 @@ const Toggler = (e) => {
 };
 
 navWrapper.addEventListener("click", Toggler);
+
+
+for (let i = 0; i < sideLinks.length; i++) {
+  sideLinks[i].addEventListener("click", () => {
+    sideNav.classList.add("hide");
+    navWrapper.classList.remove("active");
+  });
+}
